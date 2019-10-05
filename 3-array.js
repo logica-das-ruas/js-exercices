e396701cf953afc0b5ecbb7f44427f810d5dd461
marcos012 <marcospaulo.ad@gmail.com>
2019-10-05 02:46:48 -0300

    trabalhando com arrays

---
+++ 3-array.js
@@ -0,0 +1,6 @@
+function temHabilidade(skills) {
+    console.log(skills.includes('Javascript'))
+}
+
+var skills = ["Javascript", "ReactJS", "React Native"];
+temHabilidade(skills); // true ou false
