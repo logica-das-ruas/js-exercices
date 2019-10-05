a0b082f26159efb907fe16314caebf5ce1804e1d
marcos012 <marcospaulo.ad@gmail.com>
2019-10-05 03:11:37 -0300

    trabalhando com objetos dentro de um array

---
+++ 5-object-array.js
@@ -0,0 +1,15 @@
+var usuarios = [
+    {
+        nome: "Marcos",
+        habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+        nome: "Gabriel",
+        habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    }
+];
+
+
+console.log(`O ${usuarios[0].nome} possuí as habilidades ${usuarios[0].habilidades}`);
+console.log(`O ${usuarios[1].nome} possuí as habilidades ${usuarios[1].habilidades}`);
+   
